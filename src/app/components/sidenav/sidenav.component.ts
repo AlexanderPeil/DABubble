99d13cadff922e6ba2178ad3b32078b8fd21aa7b
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-08-08 12:54:22 +0200

    added hide function for chats and channels

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent {
+  channelsVisible: boolean = true;
+  chatsVisible: boolean = true;
+  arrowImageRotated: boolean = false;
 
+  hideChannels() {
+    this.channelsVisible = !this.channelsVisible;
+    this.arrowImageRotated = !this.arrowImageRotated;
+  }
+  hideChats() {
+    this.chatsVisible = !this.chatsVisible;
+    this.arrowImageRotated = !this.arrowImageRotated;
+  }
 }
