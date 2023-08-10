c8b08abbc0ecb6e3bd938329dc27d91411cadff4
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-08-10 22:05:08 +0200

    Added function to load user-data into edit-profile.comp

--- src/app/components/dialog-edit-profile/dialog-edit-profile.component.ts
+++ src/app/components/dialog-edit-profile/dialog-edit-profile.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { User } from 'src/app/shared/services/user';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-edit-profile',
   templateUrl: './dialog-edit-profile.component.html',
   styleUrls: ['./dialog-edit-profile.component.scss']
 })
-export class DialogEditProfileComponent {
+export class DialogEditProfileComponent implements OnInit, OnDestroy {
+  user?: User;
+  userForm!: FormGroup;
+  private userSubscription?: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder) { }
 
 
+  ngOnInit() {
+    this.initForm();
+
+    this.userSubscription = this.authService.user$.subscribe(firebaseUser => {
+      if (firebaseUser) {
+        this.authService.getUserData(firebaseUser.uid).subscribe(userData => {
+          this.user = userData;
+          this.userForm.patchValue(userData);
+        });
+      }
+    });
+  }
+
+
+  private initForm() {
+    this.userForm = this.fb.group({
+      displayName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+
+  onSubmit() {
+
+  }
+
+
+  ngOnDestroy() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
 
 }
