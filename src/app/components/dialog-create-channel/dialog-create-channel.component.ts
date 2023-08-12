4aff843bf2300a6c19aa82476faf1462b5433a90
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-08-12 12:35:43 +0200

    created channel.service and addChannel() with firestore

--- src/app/components/dialog-create-channel/dialog-create-channel.component.ts
+++ src/app/components/dialog-create-channel/dialog-create-channel.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  addDoc,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Channel } from 'src/app/models/channel';
 
 @Component({
   selector: 'app-dialog-create-channel',
   templateUrl: './dialog-create-channel.component.html',
-  styleUrls: ['./dialog-create-channel.component.scss']
+  styleUrls: ['./dialog-create-channel.component.scss'],
 })
 export class DialogCreateChannelComponent {
+  channel: Channel = new Channel();
+  item$: Observable<any[]>;
+  firestore: Firestore = inject(Firestore);
 
+  channelName!: string;
+  channelDescription!: string;
+
+  constructor() {
+    const channelCollection = collection(this.firestore, 'channels');
+    this.item$ = collectionData(channelCollection);
+  }
+
+  addChannel() {
+    const channelCollection = collection(this.firestore, 'channels');
+
+    addDoc(channelCollection, this.channel.toJSON()).then((result) => {
+      console.log('Adding user finished', result);
+    });
+  }
 }
--- src/app/models/channel.ts
+++ src/app/models/channel.ts
@@ -1,12 +1,19 @@
 export class Channel {
-    channelName: string;
-    channelDescription: string;
-    createDate: number;
+  channelName: string;
+  channelDescription: string;
+  createDate: number;
 
+  constructor(object?: any) {
+    this.channelName = object ? object.channelName : '';
+    this.channelDescription = object ? object.channelDescription : '';
+    this.createDate = object ? object.createDate : null;
+  }
 
-    constructor(object?: any) {
-        this.channelName = object ? object.channelName : '';
-        this.channelDescription = object ? object.channelDescription : '';
-        this.createDate = object ? object.createDate : null;
-    }
+  public toJSON() {
+    return {
+      channelName: this.channelName,
+      channelDescription: this.channelDescription,
+      createDate: this.createDate,
+    };
+  }
 }
