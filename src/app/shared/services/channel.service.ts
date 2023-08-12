330f6f4bc985e5d6f30cf11c7fd4452477303cc0
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-08-12 21:08:02 +0200

    added getChannel for sidenav so channels can be displayed from firebase

--- src/app/shared/services/channel.service.ts
+++ src/app/shared/services/channel.service.ts
@@ -9,14 +9,23 @@ import { Observable } from 'rxjs';
 import { Channel } from 'src/app/models/channel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChannelService {
+  channelData!: Observable<any>;
+
   private channelCollection = collection(this.firestore, 'channels');
 
-  constructor(private firestore: Firestore) { }
+  constructor(private firestore: Firestore) {}
 
   createChannel(channel: Channel) {
     return addDoc(this.channelCollection, channel.toJSON());
+  }
+
+  getChannel() {
+    collectionData(this.channelCollection).subscribe((channels) => {
+      console.log(channels);
+    });
+    this.channelData = collectionData(this.channelCollection);
   }
 }
