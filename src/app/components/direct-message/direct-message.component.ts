2ffe27ff43596a2ce19875050676867e8cc9dd6f
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-08-16 10:23:46 +0200

    Added explanation to the direct-message onInit

--- src/app/components/direct-message/direct-message.component.ts
+++ src/app/components/direct-message/direct-message.component.ts
@@ -28,35 +28,37 @@ export class DirectMessageComponent implements OnInit, OnDestroy {
 
 
   ngOnInit() {
+    // Observable to get the id of the selected user (selected in the sidenav).  
     const selectedUser$ = this.route.paramMap.pipe(
-      map(params => params.get('id')),
-      filter(uid => !!uid),
-      switchMap(uid => this.authService.getUserData(uid as string))
+      map(params => params.get('id')), // "map": extracts the id from the route parameters.
+      filter(uid => !!uid), // "filter": ensures that only true (non-falsy) uid values are passed through.
+      switchMap(uid => this.authService.getUserData(uid as string)) // "switchMap": each UID that passes through the filter the authservice is invoked to fetch user data
     );
 
+    // Observable to get the id of the logged-in user who selects the an user from the sidenav to chat with
     const loggedInUser$ = this.authService.user$.pipe(
-      tap(firebaseUser => {
+      tap(firebaseUser => { // "tap": checks if firebaseUser has a uid and set this.loggedInUser to null if it doesn't
         if (!firebaseUser?.uid) {
           this.loggedInUser = null;
         }
       }),
-      switchMap(firebaseUser => firebaseUser?.uid ? this.authService.getUserData(firebaseUser.uid) : []),
-      filter(user => !!user)
+      switchMap(firebaseUser => firebaseUser?.uid ? this.authService.getUserData(firebaseUser.uid) : []), // "switchMap": each UID that passes through the filter the authservice is invoked to fetch user data
+      filter(user => !!user) // This ensures that only true (non-falsy) uid values are passed through.
     );
-
+      // Combines the two observables (logged-in user and selected user)
     combineLatest([selectedUser$, loggedInUser$])
-      .pipe(takeUntil(this.ngUnsubscribe))
+      .pipe(takeUntil(this.ngUnsubscribe)) // "takeUntil": makes sure the observable completes when another obserable (ngUnsubscribe) emits a value. ngUnsubscribe is for the onDestroy
       .subscribe(([selectedUser, loggedInUser]) => {
         this.selectedUser = selectedUser;
         this.loggedInUser = loggedInUser;
-
+        // If both users have valid values then fetches the direct messages with "this.directMessageService.getDirectMessages"
         if (loggedInUser && selectedUser) {
           this.directMessageService.getDirectMessages(loggedInUser.uid, selectedUser.uid)
             .pipe(takeUntil(this.ngUnsubscribe))
             .subscribe((messages: any[]) => {
               this.messages = messages
-                .map(msg => new DirectMessageContent(msg))
-                .sort((a, b) => b.timestamp - a.timestamp);
+                .map(msg => new DirectMessageContent(msg)) // This ensures that all the messages in this.messages are instances of DirectMessageContent
+                .sort((a, b) => b.timestamp - a.timestamp); // Sorts the messages by date
             });
         } else {
           console.error("Either loggedInUser or selectedUser is null");
