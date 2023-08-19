bffb5ec6744a9a07425a137b58f715a5ab9753c1
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-08-19 13:42:05 +0200

    added function to add users to the channel

--- src/app/components/dialog-create-channel/dialog-create-channel.component.ts
+++ src/app/components/dialog-create-channel/dialog-create-channel.component.ts
@@ -5,11 +5,13 @@ import {
   collection,
   addDoc,
 } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Observable, map, switchMap } from 'rxjs';
 import { Channel } from 'src/app/models/channel';
 import { MatDialog } from '@angular/material/dialog';
 import { ChannelService } from 'src/app/shared/services/channel.service';
 import { MatRadioChange } from '@angular/material/radio';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { User } from 'src/app/shared/services/user';
 
 @Component({
   selector: 'app-dialog-create-channel',
@@ -24,6 +26,7 @@ export class DialogCreateChannelComponent {
 
   constructor(
     private channelService: ChannelService,
+    private authService: AuthService,
     public dialog: MatDialog
   ) {}
 
@@ -40,7 +43,45 @@ export class DialogCreateChannelComponent {
     }
   }
 
-  onSubmit(channel: any) {
-    this.channelService.addChannelService(channel);
+  // onSubmit(channel: any) {
+  //   this.channelService.addChannelService(channel);
+  // }
+
+  async addAllMembers() {
+    this.authService
+      .getUsers()
+      .pipe(
+        map((allUsersData) =>
+          allUsersData.map(
+            (data) =>
+              ({
+                uid: data['uid'],
+                email: data['email'],
+                displayName: data['displayName'],
+                emailVerified: data['emailVerified'],
+                isOnline: data['isOnline'],
+                photoURL: data['photoURL'],
+              } as User)
+          )
+        )
+      )
+      .subscribe((allUsers) => {
+        const users = allUsers.map((user) => user.uid);
+
+        // Add the user IDs to the channel object
+        this.channel.users = users;
+      });
+  }
+
+  async onSubmitWithMembers(channel: any) {
+    if (this.radioSelected && this.channel.users === undefined) {
+      await this.addAllMembers(); // Call addAllMembers only if radioSelected and user IDs are not defined
+
+      // Call the addChannelService method to create the channel
+      this.channelService.addChannelService(this.channel);
+    } else {
+      // Call the addChannelService method to create the channel without adding all members
+      this.channelService.addChannelService(this.channel);
+    }
   }
 }
--- src/app/models/channel.ts
+++ src/app/models/channel.ts
@@ -2,11 +2,13 @@ export class Channel {
   channelName: string;
   channelDescription: string;
   createDate: number;
+  users: any;
 
   constructor(object?: any) {
     this.channelName = object ? object.channelName : '';
     this.channelDescription = object ? object.channelDescription : '';
     this.createDate = object ? object.createDate : null;
+    this.users = object ? object.users : null;
   }
 
   toJSON() {
@@ -14,6 +16,7 @@ export class Channel {
       channelName: this.channelName,
       channelDescription: this.channelDescription,
       createDate: this.createDate,
+      users: this.users,
     };
   }
 }
