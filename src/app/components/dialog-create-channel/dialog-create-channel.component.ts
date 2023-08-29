aa7a74bba953d58b68a774e1fee2f266858b7ee7
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-08-29 15:19:15 +0200

    added first function for user add to channel  in dialogCreateChannelComponent

--- src/app/components/dialog-create-channel/dialog-create-channel.component.ts
+++ src/app/components/dialog-create-channel/dialog-create-channel.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   Firestore,
   collectionData,
@@ -18,17 +18,23 @@ import { User } from 'src/app/shared/services/user';
   templateUrl: './dialog-create-channel.component.html',
   styleUrls: ['./dialog-create-channel.component.scss'],
 })
-export class DialogCreateChannelComponent {
+export class DialogCreateChannelComponent implements OnInit {
   channel: Channel = new Channel();
   showForm = true;
-  radioSelected = false;
-  isInputVisible = false;
+  radioSelected: boolean = false;
+  isInputVisible: boolean = false;
+  showUserDropdown: boolean = false;
+  foundUsers: User[] = [];
 
   constructor(
     private channelService: ChannelService,
     private authService: AuthService,
     public dialog: MatDialog
-  ) { }
+  ) {}
+
+  ngOnInit() {
+    this.filterUsers();
+  }
 
   toggleForms() {
     this.showForm = !this.showForm;
@@ -43,10 +49,6 @@ export class DialogCreateChannelComponent {
     }
   }
 
-  // onSubmit(channel: any) {
-  //   this.channelService.addChannelService(channel);
-  // }
-
   async addAllMembers() {
     this.authService
       .getUsers()
@@ -54,14 +56,14 @@ export class DialogCreateChannelComponent {
         map((allUsersData) =>
           allUsersData.map(
             (data) =>
-            ({
-              uid: data['uid'],
-              email: data['email'],
-              displayName: data['displayName'],
-              emailVerified: data['emailVerified'],
-              isOnline: data['isOnline'],
-              photoURL: data['photoURL'],
-            } as User)
+              ({
+                uid: data['uid'],
+                email: data['email'],
+                displayName: data['displayName'],
+                emailVerified: data['emailVerified'],
+                isOnline: data['isOnline'],
+                photoURL: data['photoURL'],
+              } as User)
           )
         )
       )
@@ -71,6 +73,32 @@ export class DialogCreateChannelComponent {
         // Add the user IDs to the channel object
         this.channel.users = users;
       });
+  }
+
+  checkForDropdown(event: any): void {
+    const value = event.target.value;
+    if (value) {
+      this.showUserDropdown = true;
+      this.filterUsers(value);
+    } else {
+      this.showUserDropdown = false;
+      this.filterUsers();
+    }
+  }
+
+  filterUsers(query?: string): void {
+    this.authService.getUsers(query).subscribe((users) => {
+      this.foundUsers = users;
+    });
+  }
+
+  selectUser(user: User): void {
+    this.showUserDropdown = false;
+
+    if (!this.channel.users) {
+      this.channel.users = []; // Initialisieren Sie das Array, wenn es noch nicht existiert
+    }
+    this.channel.users.push(user.uid);
   }
 
   async onSubmitWithMembers(channel: any) {
