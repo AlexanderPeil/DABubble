27785ab6f40ea2d379bb3686c194c81851e4136d
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-08-30 16:47:34 +0200

    changed code to variable at members in channel components

--- src/app/components/dialog-show-members-in-channel/dialog-show-members-in-channel.component.ts
+++ src/app/components/dialog-show-members-in-channel/dialog-show-members-in-channel.component.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DialogAddMembersInChannelComponent } from '../dialog-add-members-in-channel/dialog-add-members-in-channel.component';
-
+import { ChannelService } from 'src/app/shared/services/channel.service';
 
 @Component({
   selector: 'app-dialog-show-members-in-channel',
   templateUrl: './dialog-show-members-in-channel.component.html',
-  styleUrls: ['./dialog-show-members-in-channel.component.scss']
+  styleUrls: ['./dialog-show-members-in-channel.component.scss'],
 })
-
-
 export class DialogShowMembersInChannelComponent {
-
-
-  constructor(public dialog: MatDialog, public dialogRef: MatDialogRef<DialogShowMembersInChannelComponent>) {
-
-  }
-
+  constructor(
+    public dialog: MatDialog,
+    public channelService: ChannelService,
+    public dialogRef: MatDialogRef<DialogShowMembersInChannelComponent>
+  ) {}
 
   openDialogToAddMembersToChannel() {
     this.dialog.open(DialogAddMembersInChannelComponent);
