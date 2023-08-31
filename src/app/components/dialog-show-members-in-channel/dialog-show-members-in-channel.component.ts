74b6b3cf16c1f6749d49051620958fbf5091c8d7
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-08-31 12:43:09 +0200

    trying to show all members of the channel in dialogShowMembersInChannel first try

--- src/app/components/dialog-show-members-in-channel/dialog-show-members-in-channel.component.ts
+++ src/app/components/dialog-show-members-in-channel/dialog-show-members-in-channel.component.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DialogAddMembersInChannelComponent } from '../dialog-add-members-in-channel/dialog-add-members-in-channel.component';
 import { ChannelService } from 'src/app/shared/services/channel.service';
+import { User } from 'src/app/shared/services/user';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-dialog-show-members-in-channel',
   templateUrl: './dialog-show-members-in-channel.component.html',
   styleUrls: ['./dialog-show-members-in-channel.component.scss'],
 })
-export class DialogShowMembersInChannelComponent {
+export class DialogShowMembersInChannelComponent implements OnInit {
+  allUsers: User[] = [];
+
   constructor(
     public dialog: MatDialog,
     public channelService: ChannelService,
+    public authService: AuthService,
     public dialogRef: MatDialogRef<DialogShowMembersInChannelComponent>
   ) {}
+
+  ngOnInit(): void {
+    this.getAllUsersFromChannel();
+  }
+
+  getAllUsersFromChannel(): void {
+    this.authService.getUsers().subscribe((users) => {
+      this.allUsers = users;
+    });
+  }
 
   openDialogToAddMembersToChannel() {
     this.dialog.open(DialogAddMembersInChannelComponent);
