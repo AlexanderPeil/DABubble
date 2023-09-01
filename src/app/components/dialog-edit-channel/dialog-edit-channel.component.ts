c5b81af2ca2e49303ac6923ee5af319111a08904
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-09-01 09:57:20 +0200

    added channel delete button

--- src/app/components/dialog-edit-channel/dialog-edit-channel.component.ts
+++ src/app/components/dialog-edit-channel/dialog-edit-channel.component.ts
@@ -4,14 +4,11 @@ import { ChannelService } from 'src/app/shared/services/channel.service';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-dialog-edit-channel',
   templateUrl: './dialog-edit-channel.component.html',
-  styleUrls: ['./dialog-edit-channel.component.scss']
+  styleUrls: ['./dialog-edit-channel.component.scss'],
 })
-
-
 export class DialogEditChannelComponent {
   showInputToEditChannelName: boolean = false;
   showInputToEditChannelDescription: boolean = false;
@@ -20,47 +17,55 @@ export class DialogEditChannelComponent {
   changedChannelDescription: string = '';
   channelNameHasChanged: boolean = false;
 
-
-  constructor(public router: Router, public channelService: ChannelService, @Inject(MAT_DIALOG_DATA) public data: {
-    channelName: string, channelId: string, channelDescription: string
-  }) {
-
-  }
-
+  constructor(
+    public router: Router,
+    public channelService: ChannelService,
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      channelName: string;
+      channelId: string;
+      channelDescription: string;
+    }
+  ) {}
 
   openInputToEditChannelName() {
     this.showInputToEditChannelName = true;
   }
-
 
   getValueForNewChannelName($event: any) {
     this.channelNameHasChanged = true;
     this.changedChannelName = $event.toLowerCase();
   }
 
-
   closeInputToEditChannelNameAndSafeChanges() {
     this.showInputToEditChannelName = false;
     if (!this.showInputToEditChannelName) {
-      this.channelService.updateChannelNameService(this.changedChannelName, this.data.channelId); // Get the ChannelId from @Inject in the Constructor.
+      this.channelService.updateChannelNameService(
+        this.changedChannelName,
+        this.data.channelId
+      ); // Get the ChannelId from @Inject in the Constructor.
     }
   }
-
 
   openInputToEditChannelDescription() {
     this.showInputToEditChannelDescription = true;
   }
 
-
   getValueForNewChannelDescription($event: any) {
     this.changedChannelDescription = $event;
   }
 
-
   closeInputToEditChannelDescriptionAndSafeChanges() {
     this.showInputToEditChannelDescription = false;
     if (!this.showInputToEditChannelDescription) {
-      this.channelService.updateChannelDescriptionService(this.changedChannelDescription, this.data.channelId); // Get the ChannelId from @Inject in the Constructor.
+      this.channelService.updateChannelDescriptionService(
+        this.changedChannelDescription,
+        this.data.channelId
+      ); // Get the ChannelId from @Inject in the Constructor.
     }
+  }
+
+  leaveChannel() {
+    this.channelService.deleteChannelService(this.data.channelId);
   }
 }
--- src/app/shared/services/channel.service.ts
+++ src/app/shared/services/channel.service.ts
@@ -10,6 +10,7 @@ import {
   onSnapshot,
   orderBy,
   query,
+  deleteDoc,
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Channel } from 'src/app/models/channel';
@@ -21,48 +22,48 @@ export class ChannelService {
   channelData!: Observable<any>;
   channel: any = new Channel();
 
-
-  constructor(private firestore: Firestore) {
-
-  }
-
+  constructor(private firestore: Firestore) {}
 
   addChannelService(channel: any) {
     const collectionInstance = collection(this.firestore, 'channels');
     addDoc(collectionInstance, channel.toJSON());
   }
 
-
   getChannelService() {
-    const collectionInstance = query(collection(this.firestore, 'channels'), orderBy('channelName'));
+    const collectionInstance = query(
+      collection(this.firestore, 'channels'),
+      orderBy('channelName')
+    );
     this.channelData = collectionData(collectionInstance, { idField: 'id' });
   }
 
-
-  updateChannelNameService(changedChannelName: string, channelId: string,) {
+  updateChannelNameService(changedChannelName: string, channelId: string) {
     const docInstance = doc(this.firestore, 'channels', channelId);
     const updateData = {
       channelName: changedChannelName,
-    }
+    };
     updateDoc(docInstance, updateData);
     window.location.reload();
   }
 
-
-  updateChannelDescriptionService(changedChannelDescription: string, channelId: string) {
+  updateChannelDescriptionService(
+    changedChannelDescription: string,
+    channelId: string
+  ) {
     const docInstance = doc(this.firestore, 'channels', channelId);
     const updateData = {
       channelDescription: changedChannelDescription,
-    }
+    };
     updateDoc(docInstance, updateData);
     window.location.reload();
   }
 
-
-  deleteChannelService() {
-
+  deleteChannelService(channelId: string) {
+    const docInstance = doc(this.firestore, 'channels', channelId);
+    deleteDoc(docInstance).then(() => {
+      console.log('Document deleted');
+    });
   }
-
 
   getSingleChannelService(channelId: string) {
     const collectionInstance = collection(this.firestore, 'channels');
