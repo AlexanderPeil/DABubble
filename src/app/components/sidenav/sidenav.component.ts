37769235148894d5cea36facbb215e56298e176d
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-10-17 14:31:03 +0200

    first responsive sidenav with searchbar

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogCreateChannelComponent } from '../dialog-create-channel/dialog-create-channel.component';
 import { AuthService } from 'src/app/shared/services/auth.service';
@@ -8,13 +8,13 @@ import { Observable, Subscription, take, tap } from 'rxjs';
 import { Router } from '@angular/router';
 import { MessageService } from 'src/app/shared/services/message.service';
 
-
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit, OnDestroy {
+  @HostListener('document:click', ['$event'])
   channelsVisible: boolean = true;
   chatsVisible: boolean = true;
   arrowImageRotatedChannel: boolean = false;
@@ -22,24 +22,31 @@ export class SidenavComponent implements OnInit, OnDestroy {
   users: User[] = [];
   userSubscription!: Subscription;
   isOnline?: boolean;
-  loggedInUser: Observable<User | null> = this.authService.currentUser.asObservable();
+  loggedInUser: Observable<User | null> =
+    this.authService.currentUser.asObservable();
   user_images = '../assets/img/avatar1.svg';
-
+  screenWidth!: number;
 
   constructor(
     public dialog: MatDialog,
     public authService: AuthService,
     public channelService: ChannelService,
     private router: Router,
-    private messageService: MessageService,
-  ) { }
-
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
-    this.userSubscription = this.authService.getUsers().subscribe(users => this.users = users);
+    this.userSubscription = this.authService
+      .getUsers()
+      .subscribe((users) => (this.users = users));
     this.channelService.getChannelService();
+    this.screenWidth = window.innerWidth;
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.screenWidth = window.innerWidth;
+  }
 
   retryLoadImage(user: User) {
     if (user) {
@@ -47,44 +54,43 @@ export class SidenavComponent implements OnInit, OnDestroy {
     }
   }
 
-
   hideChannels() {
     this.channelsVisible = !this.channelsVisible;
     this.arrowImageRotatedChannel = !this.arrowImageRotatedChannel;
   }
-
 
   hideChats() {
     this.chatsVisible = !this.chatsVisible;
     this.arrowImageRotatedChat = !this.arrowImageRotatedChat;
   }
 
-
   openDialogToCreateChannel() {
     this.dialog.open(DialogCreateChannelComponent);
   }
 
-
   onUserClick(clickedUser: User) {
-    this.loggedInUser.pipe(
-      take(1),
-      tap(loggedInUser => {
-        if (loggedInUser && loggedInUser.uid !== clickedUser.uid) {
-          if (loggedInUser.hasUnreadMessages?.includes(clickedUser.uid)) {
-            this.messageService.markMessagesAsRead(clickedUser.uid, loggedInUser.uid);
+    this.loggedInUser
+      .pipe(
+        take(1),
+        tap((loggedInUser) => {
+          if (loggedInUser && loggedInUser.uid !== clickedUser.uid) {
+            if (loggedInUser.hasUnreadMessages?.includes(clickedUser.uid)) {
+              this.messageService.markMessagesAsRead(
+                clickedUser.uid,
+                loggedInUser.uid
+              );
+            }
           }
-        }
-      })
-    ).subscribe();
+        })
+      )
+      .subscribe();
 
     this.router.navigate(['main', 'direct-message', clickedUser.uid]);
   }
 
-
   onChannelClick(channelId: string) {
     this.messageService.markChannelMessageAsRead(channelId);
   }
-
 
   ngOnDestroy() {
     this.userSubscription?.unsubscribe();
